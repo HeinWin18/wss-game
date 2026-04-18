ff76cb8982d9d4ceda7079ba5ded266ae6a113c4
cklam48 <ChrisKLam93@gmail.com>
2026-04-18 13:03:17 -0700

    Add Square and Map java files

--- Map.java
+++ Map.java
@@ -1,5 +1,39 @@
+package wss;
+
 public class Map {
-    public Square getSquare(int x, int y) {
-        return null;
-    }
+
+	private int rows;
+	private int cols;
+	private int difficulty;
+	private Square[][] squares;
+
+	// Constructor (20x20 size matrix for now)
+	public Map(int difficulty) {
+		this.rows = 20;
+		this.cols = 20;
+		this.difficulty = difficulty;
+		this.squares = new Square[rows][cols];
+	}
+
+	// Getter methods
+	public int getRows() {
+		return rows;
+	}
+
+	public int getCols() {
+		return cols;
+	}
+
+	public Square getSquare(int x, int y) {
+		return squares[x][y];
+	}
+
+	// Fills the grid with Square objects
+	public void populate(int difficulty) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				squares[i][j] = new Square(i, j);
+			}
+		}
+	}
 }
--- Square.java
+++ Square.java
@@ -1,1 +1,42 @@
-public class Square {}
+package wss;
+
+public class Square {
+
+	// private Terrain terrain;
+	// private Item item;
+	// private Trader trader;
+	private int x;
+	private int y;
+
+	// Constructor
+	public Square(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	// Getter methods
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	// Stubbed methods - to be implemented later
+
+	/*
+	 * 
+	 * public Terrain getTerrain() { return terrain; }
+	 * 
+	 * public Item getItem() { return item; }
+	 * 
+	 * public Trader getTrader() { return trader; }
+	 * 
+	 * public boolean hasItem() { return false; }
+	 * 
+	 * public boolean hasTrader() { return false; }
+	 * 
+	 */
+
+}
