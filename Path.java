ff235bdebe4d1906784a3aa557234eb96db9d6d8
Justin Win <121010724+HeinWin18@users.noreply.github.com>
2026-04-29 12:54:00 -0700

    chore: merged Aldo's Path class with cost attributes and updated to use Log system

--- Path.java
+++ Path.java
@@ -2,21 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Placeholder Path class for Week 1 compilation.
- * Aldo and Parker will fully implement this later.
+ * Represents a sequence of moves and the accumulated costs to travel them.
  */
-
 public class Path {
+
     private List<Move> moves;
+    private int totalCost;
+    private int waterCost;
+    private int foodCost;
 
     /**
      * Basic constructor.
      * Inputs: None
      * Outputs: Path object
      */
-    public Path(){
+    public Path() {
         Log.methodStart("Path", "Constructor", "none");
         this.moves = new ArrayList<>();
+        this.totalCost = 0;
+        this.waterCost = 0;
+        this.foodCost = 0;
         Log.methodEnd("Path", "Constructor", "void");
     }
 
@@ -25,13 +30,11 @@ public class Path {
      * Inputs: move (Move)
      * Outputs: None
      */
-    public void addMove(Move move){
-        Log.methodStart("Path", "addMove", "Move: " + move.getDirection());
+    public void addMove(Move move) {
+        Log.methodStart("Path", "addMove", "Move(" + move.getDirection() + ")");
         this.moves.add(move);
         Log.methodEnd("Path", "addMove", "void");
     }
-
-
 
     /**
      * Returns the sequence of moves.
@@ -40,7 +43,43 @@ public class Path {
      */
     public List<Move> getMoves() {
         Log.methodStart("Path", "getMoves", "none");
-        Log.methodEnd("Path", "getMoves", "List of " + moves.size() + " moves");
+        Log.methodEnd("Path", "getMoves", "List<Move> size: " + moves.size());
         return moves;
+    }
+
+    // Getters
+    public int getTotalCost() {
+        Log.methodStart("Path", "getTotalCost", "none");
+        Log.methodEnd("Path", "getTotalCost", String.valueOf(totalCost));
+        return totalCost;
+    }
+
+    public int getWaterCost() {
+        Log.methodStart("Path", "getWaterCost", "none");
+        Log.methodEnd("Path", "getWaterCost", String.valueOf(waterCost));
+        return waterCost;
+    }
+
+    public int getFoodCost() {
+        Log.methodStart("Path", "getFoodCost", "none");
+        Log.methodEnd("Path", "getFoodCost", String.valueOf(foodCost));
+        return foodCost;
+    }
+
+    /**
+     * Logs the sequence of moves in the path.
+     * Inputs: None
+     * Outputs: None
+     */
+    public void printPath() {
+        Log.methodStart("Path", "printPath", "none");
+        
+        StringBuilder pathString = new StringBuilder("Path Moves: ");
+        for (Move m : moves) {
+            pathString.append(m.getDirection()).append(" ");
+        }
+        Log.info(pathString.toString().trim());
+        
+        Log.methodEnd("Path", "printPath", "void");
     }
 }
