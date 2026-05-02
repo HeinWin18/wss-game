01faa0ec4a903510f2796b60f0f63a96b2306e36
Justin Win <121010724+HeinWin18@users.noreply.github.com>
2026-05-01 19:24:21 -0700

    WIP: GameManager update loop (commented out broken map populate)

--- GameManager.java
+++ GameManager.java
@@ -59,11 +59,30 @@ public class GameManager {
                 //4. Execute move (Player class)
                 player.move(move);
 
-                //5. Print the new coordinates and stats
-                player.printStatus();
+                // //5. Week 3 Integration logic: Apply terrain costs, item pickups.
+                // // Get the exact square the player landed on
+                // Square currentSquare = map.getSquare(player.getX(), player.getY());
 
-                //Get the square to prep for future terrain/trader/item interactions
-                map.getSquare(player.getX(), player.getY());
+                // // Apply terrain costs
+                // Terrain currentTerrain = currentSquare.getTerrain();
+                // if (currentTerrain != null){
+                //     currentTerrain.applyEffect(player); // This will modify the player's stats based on the terrain type
+                //     Log.info("Stepped on " + currentTerrain.getClass().getSimpleName() + ", applied effect.");
+                // }
+
+                // //Check for and Collect Items
+                // if (currentSquare.hasItem()){
+                //     Item item  = currentSquare.getItem();
+                //     //need to add collectItem(Item i) to the player class
+                //     player.collectItem(item);
+                //     Log.info("Collected item: " + item.getClass().getSimpleName() + "!");
+
+                //     // TODO: Remove item from the square after collection
+                //      //currentSquare.removeItem();
+                // }
+
+                // //Get the square to prep for future terrain/trader/item interactions
+                // map.getSquare(player.getX(), player.getY());
 
             } catch (Exception e) {
                 Log.error("Error occurred: " + e.getMessage());
